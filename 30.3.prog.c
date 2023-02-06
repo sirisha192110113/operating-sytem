04215850844d891065e45783a585fdcc2d3ad430
sirisha192110113 <113408515+sirisha192110113@users.noreply.github.com>
2023-02-06 06:24:56 +0530

    Create 30.3.prog.c

---
+++ 30.3.prog.c
@@ -0,0 +1,12 @@
+#include <pthread.h> 
+ #include <stdio.h> 
+  
+ int main () 
+ { 
+    pthread_t thread1, thread2; 
+  
+    printf("Before thread creation, thread1 = %ld, thread2 = %ld\n", thread1, thread2); 
+  
+    pthread_create; 
+    return 0; 
+    }
